afbbefa11f8ba557dc56128aadddae5527195241
yuma300 <ywakimoto2s@gmail.com>
2015-04-29 23:32:15 +0900

    インデント整形

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,8 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each { |f| require f }
 RSpec.configure do |config|
-config.mock_with :rspec
-config.use_transactional_fixtures = true
-config.infer_base_class_for_anonymous_controllers = false
-config.order = "random"
+  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
 end
